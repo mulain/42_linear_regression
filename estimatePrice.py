9f91569ec40c37cb1b13c7f32ad561d620788f64
Wolf Arif Mardin <wolf.mardin@gmail.com>
2024-04-25 12:00:55 +0200

    02

--- estimatePrice.py
+++ estimatePrice.py
@@ -1,11 +1,13 @@
 import numpy as np
 import pandas as pd
 
-theta0 = 0.0
-theta1 = 0.0
-dataFrame = pd.read_csv('data.csv')
-mileages = dataFrame['km'].values
-prices = dataFrame['price'].values
+def main():
+    theta0 = 0.0
+    theta1 = 0.0
+    
+    dataFrame = pd.read_csv('data.csv')
+    mileages = dataFrame['km'].values
+    prices = dataFrame['price'].values
 
 def estimatePrice(mileage, theta0, theta1):
     return theta0 + (theta1 * mileage)
